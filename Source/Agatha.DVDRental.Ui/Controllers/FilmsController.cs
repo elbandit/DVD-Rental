906cb8f763f7b966c14efa75dfce4156f874a52c
Scott Millett <scott@wiggle.co.uk>
2013-04-21 16:15:38 +0100

    Mocking out customer store front.

---
+++ Source/Agatha.DVDRental.Ui/Controllers/FilmsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Agatha.DVDRental.Ui.Controllers
+{
+    public class FilmsController : Controller
+    {
+        //
+        // GET: /Films/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Source/Agatha.DVDRental.Ui/Controllers/RentalListController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Agatha.DVDRental.Ui.Controllers
+{
+    public class RentalListController : Controller
+    {
+        //
+        // GET: /RentalList/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
