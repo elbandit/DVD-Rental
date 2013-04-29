e3d9042c1adc5a5f0a2334705b094a1c6a375a70
Scott Millett <scott@wiggle.co.uk>
2013-04-29 20:48:12 +0100

    More stubbing out of business cases

---
+++ Source/Agatha.DVDRental.ApplicationService/SubscriptionService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Agatha.DVDRental.ApplicationService
+{
+    public class SubscriptionService
+    {
+
+        public void CustomerWantsToCancelSubscription(int subscriptionId)
+        {
+
+        }
+
+        public void CustomerWantsToChangeSubscription(int subscriptionId)
+        {
+
+        }
+
+        public void CustomerWantsToTakeAPaymentHoliday(int subscriptionId)
+        {
+
+        }
+    }
+}
